a1d731f0ae8b9238c24531a58540b5073814a752
brunosc10699 <brunosc10699@gmail.com>
2021-05-12 21:44:59 -0300

    Home, Navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,6 +1,24 @@
+import Footer from "components/Footer";
+import Navbar from "components/Navbar";
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
-        <div>Home</div>
+        <>
+            <Navbar />
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4">DSVendas</h1>
+                    <p className="lead">Analise o desempenho das suas vendas por diferentes perpectivas</p>
+                    <hr/>
+                    <p>Esta aplicação consiste em exibir um dashbaoard a partir de dados fornecidos por um back end contruído com Spring Boot.</p>
+                    <Link className="btn btn-primary btn-lg" to="/dashboard">
+                        Veja o Dashboard
+                    </Link>
+                </div>    
+            </div>
+            <Footer />
+        </>
     );
 }
 
